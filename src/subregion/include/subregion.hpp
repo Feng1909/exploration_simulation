0d030c22ab1c19bf7d58e05502406ab886f22ec4
Yunji.Feng <732792434@qq.com>
2024-03-15 16:33:01 +0800

    finish subregion division

--- src/subregion/include/subregion.hpp
+++ src/subregion/include/subregion.hpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <visualization_msgs/Marker.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Point.h>
 #include <ros/ros.h>
+
+enum subregion_status
+{
+    UNEXPLORED,
+    EXPLORING,
+    EXPLORED,
+    OBSTACLE
+};
 
 struct subregion_square
 {
-    double x, y;
-    bool explored = false;
+    double left_down_x, left_down_y;
+    double right_up_x, right_up_y;
+    enum subregion_status states = UNEXPLORED;
 };
 
 
@@ -16,12 +27,25 @@ double init_x = 0;
 double init_y = 0;
 double resolution = 0;
 std::vector<subregion_square> squares;
+nav_msgs::Odometry car_state_1, car_state_2;
 
 ros::Publisher marker_pub;
+ros::Publisher subregion_pub;
 ros::Subscriber map_sub;
+ros::Subscriber car_state_sub_1, car_state_sub_2;
+
+std::vector<double>x,y;
 
 // params
 double square_length;
+
+void car_state_callback_1(nav_msgs::Odometry msg) {
+    car_state_1 = msg;
+}
+
+void car_state_callback_2(nav_msgs::Odometry msg) {
+    car_state_2 = msg;
+}
 
 void mapCallback(nav_msgs::OccupancyGrid msg) {
     map = msg;
@@ -31,6 +55,87 @@ void mapCallback(nav_msgs::OccupancyGrid msg) {
         resolution = map.info.resolution;
         init_flag = true;
     }
+}
+
+void gen_subregion() {
+    squares.clear();
+    for (auto i=x.begin(); i!=x.end(); i++) {
+        for (auto j=y.begin(); j!=y.end(); j++) {
+            subregion_square square;
+            square.left_down_x = *i;
+            square.left_down_y = *j;
+            square.right_up_x = *i + square_length;
+            square.right_up_y = *j + square_length;
+
+            int left_down_x = floor((square.left_down_x )/resolution);
+            int left_down_y = floor((square.left_down_y)/resolution);
+            int right_up_x = ceil((square.right_up_x)/resolution);
+            int right_up_y = ceil((square.right_up_y)/resolution);
+            int unexplored_area = 0;
+            for (int i=std::max(left_down_x, 0); i<std::min(right_up_x, int(map.info.width)); i++) {
+                for (int j=std::max(left_down_y, 0); j<std::min(right_up_y, int(map.info.height)); j++) {
+                    if (map.data[i + j*map.info.width] != -1) {
+                        unexplored_area ++;
+                    }
+                }
+            }
+            double occupied = double(unexplored_area)/(right_up_x - left_down_x)/(right_up_y - left_down_y);
+            if (((car_state_1.pose.pose.position.x-init_x)/resolution > left_down_x && (car_state_1.pose.pose.position.x-init_x)/resolution < right_up_x &&
+                (car_state_1.pose.pose.position.y-init_y)/resolution > left_down_y && (car_state_1.pose.pose.position.y-init_y)/resolution < right_up_y) ||
+                ((car_state_2.pose.pose.position.x-init_x)/resolution > left_down_x && (car_state_2.pose.pose.position.x-init_x)/resolution < right_up_x &&
+                (car_state_2.pose.pose.position.y-init_y)/resolution > left_down_y && (car_state_2.pose.pose.position.y-init_y)/resolution < right_up_y))
+                square.states = EXPLORING;
+            else if (occupied > 0.9)
+                square.states = EXPLORED;
+            else if (occupied == 0)
+                square.states = OBSTACLE;
+            else {
+                square.states = UNEXPLORED;
+
+            }
+            squares.push_back(square);
+        }
+    }
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "carto_map1";
+    marker.ns = "SubRegion";
+    marker.id = 0;
+    marker.type = visualization_msgs::Marker::CUBE_LIST;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.pose.orientation.w = 1.0;
+    // Set the scale of the marker.
+    marker.scale.x = square_length;
+    marker.scale.y = square_length;
+    marker.scale.z = 0.01;
+    marker.lifetime = ros::Duration();
+    std_msgs::ColorRGBA color_explored, color_unexplored, color_obstacle, color_exploring;
+    color_explored.r = 0.2;
+    color_explored.g = 0.6;
+    color_explored.b = 0.3;
+    color_explored.a = 0.4;
+    color_unexplored = color_explored;
+    color_unexplored.a = 0.1;
+    color_obstacle = color_explored;
+    color_obstacle.r = 0.0;
+    color_obstacle.b = 0.0;
+    color_obstacle.g = 0.0;
+    color_exploring = color_explored;
+    color_exploring.a = 0.8;
+    for (auto square=squares.begin(); square!=squares.end(); square++) {
+        geometry_msgs::Point pos;
+        pos.x = (square->left_down_x + square->right_up_x)/2;
+        pos.y = (square->left_down_y + square->right_up_y)/2;
+        if (square->states == UNEXPLORED)
+            marker.colors.push_back(color_unexplored);
+        else if (square->states == OBSTACLE)
+            marker.colors.push_back(color_obstacle);
+        else if (square->states == EXPLORING)
+            marker.colors.push_back(color_exploring);
+        else
+            marker.colors.push_back(color_explored);
+        marker.points.push_back(pos);
+    }
+    subregion_pub.publish(marker);
 }
 
 void gen_square() {
@@ -45,7 +150,7 @@ void gen_square() {
                      ceil(-start_y/square_length);
     int num_square = num_width*num_height;
     visualization_msgs::Marker marker;
-    marker.header.frame_id='carto_map1';
+    marker.header.frame_id="carto_map1";
     marker.header.stamp = ros::Time::now();
     marker.ns = "grid";
     marker.id = 0;
@@ -82,6 +187,9 @@ void gen_square() {
     marker.points.push_back(start_point);
     marker.points.push_back(end_point);
 
+    x.clear();
+    y.clear();
+
     for (double i = -start_x; i < width*resolution; i += square_length) {
         start_point.x = i;
         start_point.y = left_y;
@@ -89,6 +197,7 @@ void gen_square() {
         end_point.y = end_y;
         marker.points.push_back(start_point);
         marker.points.push_back(end_point);
+        x.push_back(i);
     }
     
     for (double i = -start_x; i > -square_length; i -= square_length) {
@@ -98,6 +207,8 @@ void gen_square() {
         end_point.y = end_y;
         marker.points.push_back(start_point);
         marker.points.push_back(end_point);
+        if (i != -start_x)
+            x.push_back(i);
     }
 
     for (double i = -start_y; i < height*resolution; i += square_length) {
@@ -107,6 +218,7 @@ void gen_square() {
         end_point.y = i;
         marker.points.push_back(start_point);
         marker.points.push_back(end_point);
+        y.push_back(i);
     }
 
     for (double i = -start_y; i > -square_length; i -= square_length) {
@@ -116,6 +228,8 @@ void gen_square() {
         end_point.y = i;
         marker.points.push_back(start_point);
         marker.points.push_back(end_point);
+        if (i != -start_y)
+            y.push_back(i);
     }
 
     marker_pub.publish(marker);
--- src/subregion/src/subregion_node.cpp
+++ src/subregion/src/subregion_node.cpp
@@ -4,12 +4,16 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "subregion_node");
     ros::NodeHandle nh;
     map_sub = nh.subscribe<nav_msgs::OccupancyGrid>("/map", 1, mapCallback);
+    car_state_sub_1 = nh.subscribe<nav_msgs::Odometry>("/state_estimation1", 1, car_state_callback_1);
+    car_state_sub_2 = nh.subscribe<nav_msgs::Odometry>("/state_estimation2", 1, car_state_callback_2);
     marker_pub = nh.advertise<visualization_msgs::Marker>("/subregion_marker", 1);
+    subregion_pub = nh.advertise<visualization_msgs::Marker>("/subregion", 1);
     ros::Rate rate(10);
     nh.getParam("square_length", square_length);
     while (ros::ok()) {
         ros::spinOnce();
         gen_square();
+        gen_subregion();
         rate.sleep();
     }
 }
